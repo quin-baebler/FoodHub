e32c9a88d10d7cbf25dd14ba80540e69d3335709
崔弘一铭 <chym2002@uw.edu>
2024-03-10 04:36:49 -0700

    added layout and http request

--- app/src/main/java/edu/uw/ischool/xyou/foodhub/post/PostFragment.kt
+++ app/src/main/java/edu/uw/ischool/xyou/foodhub/post/PostFragment.kt
@@ -1,16 +1,21 @@
 package edu.uw.ischool.xyou.foodhub.post
 
+import android.content.Context
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
 import androidx.fragment.app.Fragment
+import com.android.volley.Request
+import com.android.volley.toolbox.JsonObjectRequest
+import com.android.volley.toolbox.Volley
 import edu.uw.ischool.xyou.foodhub.R
-
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
+import edu.uw.ischool.xyou.foodhub.utils.VolleyService
+import org.json.JSONObject
 
 /**
  * A simple [Fragment] subclass.
@@ -18,18 +23,9 @@ private const val ARG_PARAM2 = "param2"
  * create an instance of this fragment.
  */
 class PostFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
-    }
-
+    private val TAG = "PostFragment"
+    private val BASE_URL = "https://foodhub-backend.azurewebsites.net/api/"
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
@@ -38,23 +34,49 @@ class PostFragment : Fragment() {
         return inflater.inflate(R.layout.fragment_post, container, false)
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment PostFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            PostFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val sharedPreferences = requireActivity().getSharedPreferences("userData", Context.MODE_PRIVATE)
+        val username = sharedPreferences.getString("username", "User")
+
+        // collect user input
+        val titleInput = view.findViewById<EditText>(R.id.title_input)
+        val descrInput = view.findViewById<EditText>(R.id.descr_input)
+        val caloriesInput = view.findViewById<EditText>(R.id.calories_input)
+
+        val postButton = view.findViewById<Button>(R.id.post_button)
+
+        postButton.setOnClickListener {
+            val title = titleInput.text.toString()
+            val descr = descrInput.text.toString()
+            val calories = caloriesInput.text.toString().toInt()
+
+            postThread(username, title, descr, calories)
+        }
+    }
+
+    private fun postThread(username: String, title: String, descr: String, calories: Int) {
+        val url = "$BASE_URL/posts"
+
+        // create a json object
+        val jsonRequest = JSONObject()
+        jsonRequest.apply {
+            put("username", username)
+            put("title", title)
+            put("descr", descr)
+            put("calories", calories)
+        }
+
+        val request = JsonObjectRequest(Request.Method.POST, url, jsonRequest,
+            {response ->
+
+            },
+            { error ->
+                Log.e(TAG, "Error posting:", error)
+                Toast.makeText(requireContext(), "Operation failed, please try again", Toast.LENGTH_SHORT).show()
+            })
+
+        VolleyService.getInstance(requireActivity()).add(request)
     }
 }
